a19dd854cbb11f617f95fa0dce257bb1d9b6d9ca
Akshat <akshatjangir1710@gmail.com>
2024-04-22 23:15:42 +0530

    added more projects

---
+++ Awesome/components/Spotify/AppSix.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable semi */
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function AppSix() {
+  return (
+    <View>
+      <Text>AppSix</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
